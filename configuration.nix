9f8c8a9b8924029ae1ec7c4983c1ec332c7570b0
Steven Klaiber-Noble <steven@DESKTOP-K3M3DQD.localdomain>
2021-09-21 18:56:28 +0000

    soft link did not work

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,65 @@
-/etc/nixos/configuration.nix
+{ lib, pkgs, config, modulesPath, ... }:
+
+with lib;
+let
+  defaultUser = "steven";
+  syschdemd = import ./syschdemd.nix { inherit lib pkgs config defaultUser; };
+in {
+  imports = [ "${modulesPath}/profiles/minimal.nix" ];
+
+  # WSL is closer to a container than anything else
+  boot.isContainer = true;
+
+  environment.etc.hosts.enable = false;
+  environment.etc."resolv.conf".enable = false;
+
+  networking.dhcpcd.enable = false;
+
+  users.users.${defaultUser} = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
+  users.users.root = {
+    shell = "${syschdemd}/bin/syschdemd";
+    # Otherwise WSL fails to login as root with "initgroups failed 5"
+    extraGroups = [ "root" ];
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  # Disable systemd units that don't make sense on WSL
+  systemd.services."serial-getty@ttyS0".enable = false;
+  systemd.services."serial-getty@hvc0".enable = false;
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@".enable = false;
+
+  systemd.services.firewall.enable = false;
+  systemd.services.systemd-resolved.enable = false;
+  systemd.services.systemd-udevd.enable = false;
+
+  # Don't allow emergency mode, because we don't have a console.
+  systemd.enableEmergencyMode = false;
+
+  environment.noXlibs = lib.mkForce false;
+
+  environment.systemPackages = [
+    pkgs.neovim
+    pkgs.nixfmt
+    pkgs.tmux
+    pkgs.git
+    pkgs.gnumake
+    pkgs.python3
+    pkgs.pipenv
+    pkgs.wget
+    pkgs.nodejs-14_x
+    pkgs.autossh
+    pkgs.google-cloud-sdk
+    pkgs.fswatch
+    pkgs.nodePackages.yarn
+    pkgs.sbt
+    pkgs.awscli2
+    pkgs.nodePackages.pyright
+    pkgs.nodePackages.typescript
+  ];
+}
